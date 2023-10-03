b135a637a0694627049fcf82b8e4ebc7e2d3debf
zabusak <142234669+zabusak@users.noreply.github.com>
2023-10-03 14:43:31 +0300

    E-ticaret websitesi
    css ve html taglarıyla oluşturduğum e ticaret websitesi temelli hazırlamış olduğum websitesi (javascript,html,css)

---
+++ e ticaret websitesi/script.js
@@ -0,0 +1,14 @@
+var swiper = new swiper(".mySwiper",{
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+navigation: {
+    nextE1: ".swiper-button-next",
+    prevE1: ".swiper-button-prev",
+},
+
+
+
+
+});
